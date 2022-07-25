444816997598dce8678fc7a6b8698954cb9da9dc
Trintin Affleck <t.affleck101@hotmail.com>
2022-07-25 17:24:00 -0600

    playRound functionality and gameOn

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ function getAIChoice() {
     const paper = "Paper";
     const scissors = "Scissors";
     number = Math.floor(Math.random()*3);
-    console.log(number)
     switch (number) {
         case 0:
             console.log(`Computer choice: ${rock}`)
@@ -19,4 +18,98 @@ function getAIChoice() {
             break;
     }
 }
-getAIChoice()
+let aiCount = 0;
+let playerCount = 0;
+function playRound(playerSelection, aiSelection = "")
+{   
+    while (true) {
+    //Rock selected
+    if (playerSelection.toLowerCase() === "rock")
+    {   
+        //Player Won Round
+        if (aiSelection.toLowerCase() === "scissors")
+        {
+            playerCount++;
+            break;
+        }
+        //Ai Won Round
+        else if (aiSelection.toLowerCase() === "paper")
+        {
+            aiCount++;
+            break;
+        }
+    }
+    //Paper selected
+    if (playerSelection.toLowerCase() === "paper")
+    {
+        //Player Won Round
+        if (aiSelection.toLowerCase() === "rock")
+        {
+            playerCount++;
+            break;
+        }
+        //Ai Won Round
+        else if (aiSelection.toLowerCase() === "scissors")
+        {
+            aiCount++;
+            break;
+        }
+    }
+    //Scissors selected
+    if (playerSelection.toLowerCase() === "scissors")
+    {
+        //Player Won Round
+        if (aiSelection.toLowerCase() === "paper")
+        {
+            playerCount++;
+            break;
+        }
+        //Ai Won Round
+        else if (aiSelection.toLowerCase() === "rock")
+        {
+            aiCount++;
+            break;
+        }
+    }
+    else if (playerSelection === aiSelection)
+    {
+        aiSelection = getAIChoice();
+    }
+    }
+}
+
+function gameOn()
+{
+    for (let i = 0; i < 5; i++) 
+    {
+        let playerSelection = prompt("Player Selection: ","Rock, Paper, or Scissors");
+        //If selection is null
+        if (typeof(playerSelection === null))
+        {
+            console.log("1")
+            while (true)
+            {
+                let playerSelection = prompt("Player Selection: ","Rock, Paper, or Scissors");
+                if (typeof(playerSelection === String))
+                {
+                    console.log(typeof(playerSelection))
+                    break;
+                }
+                else
+                {   
+                    console.log(typeof(playerSelection))
+                    console.log("Please enter your choice!")
+                }
+            }
+        }
+        //Valid selection
+        else if ((playerSelection.toLowerCase() === ("rock" || "paper" || "scissors"))
+                    && (typeof(playerSelection) === String))
+        {
+            console.log("valid selection")
+            break;
+        }
+        playRound(playerSelection,getAIChoice());
+    }
+}
+gameOn()
