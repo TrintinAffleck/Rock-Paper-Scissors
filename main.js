ad7aad0ad0235fb49592d693a55629f5d0930654
Trintin Affleck <t.affleck101@hotmail.com>
2022-07-27 15:25:28 -0600

    Finished basic functionality

--- main.js
+++ main.js
@@ -1,115 +1,127 @@
 function getAIChoice() {
-    const rock = "Rock";
-    const paper = "Paper";
-    const scissors = "Scissors";
+    const rock = "rock";
+    const paper = "paper";
+    const scissors = "scissors";
     number = Math.floor(Math.random()*3);
     switch (number) {
         case 0:
-            console.log(`Computer choice: ${rock}`)
-            return rock
+            console.log(`Computer choice: ${rock}`);
+            return rock;
         case 1:
-            console.log(`Computer choice: ${paper}`)
-            return paper
+            console.log(`Computer choice: ${paper}`);
+            return paper;
         case 2:
-            console.log(`Computer choice: ${scissors}`)
-            return scissors
+            console.log(`Computer choice: ${scissors}`);
+            return scissors;
         default:
-            console.log("Ai default choice this should never happen")
+            console.log("Ai default choice this should never happen");
             break;
     }
 }
+
 let aiCount = 0;
 let playerCount = 0;
+
 function playRound(playerSelection, aiSelection = "")
 {   
     while (true) {
-    //Rock selected
-    if (playerSelection.toLowerCase() === "rock")
-    {   
-        //Player Won Round
-        if (aiSelection.toLowerCase() === "scissors")
-        {
-            playerCount++;
-            break;
+        //Rock selected
+        if (playerSelection.toLowerCase() === "rock")
+        {   
+            //Player Won Round
+            if (aiSelection.toLowerCase() === "scissors")
+            {
+                playerCount++;
+                console.log("Player wins this round!");
+                break;
+            }
+            //Ai Won Round
+            else if (aiSelection.toLowerCase() === "paper")
+            {
+                aiCount++;
+                console.log("Computer wins this round!");
+                break;
+            }
         }
-        //Ai Won Round
-        else if (aiSelection.toLowerCase() === "paper")
+        //Paper selected
+        else if (playerSelection.toLowerCase() === "paper")
         {
-            aiCount++;
-            break;
+            //Player Won Round
+            if (aiSelection.toLowerCase() === "rock")
+            {
+                playerCount++;
+                console.log("Player wins this round!");
+                break;
+            }
+            //Ai Won Round
+            else if (aiSelection.toLowerCase() === "scissors")
+            {
+                aiCount++;
+                console.log("Computer wins this round!");
+                break;
+            }
         }
-    }
-    //Paper selected
-    if (playerSelection.toLowerCase() === "paper")
-    {
-        //Player Won Round
-        if (aiSelection.toLowerCase() === "rock")
+        //Scissors selected
+        else (playerSelection.toLowerCase() === "scissors")
         {
-            playerCount++;
-            break;
+            //Player Won Round
+            if (aiSelection.toLowerCase() === "paper")
+            {
+                playerCount++;
+                console.log("Player wins this round!");
+                break;
+            }
+            //Ai Won Round
+            else if (aiSelection.toLowerCase() === "rock")
+            {
+                aiCount++;
+                console.log("Computer wins this round!");
+                break;
+            }
         }
-        //Ai Won Round
-        else if (aiSelection.toLowerCase() === "scissors")
+        //If selections are the same redo aiSelection
+        while (playerSelection.toLowerCase() === aiSelection)
         {
-            aiCount++;
-            break;
+            console.log("Choices are the same woops!")
+            aiSelection = getAIChoice();
         }
-    }
-    //Scissors selected
-    if (playerSelection.toLowerCase() === "scissors")
-    {
-        //Player Won Round
-        if (aiSelection.toLowerCase() === "paper")
-        {
-            playerCount++;
-            break;
-        }
-        //Ai Won Round
-        else if (aiSelection.toLowerCase() === "rock")
-        {
-            aiCount++;
-            break;
-        }
-    }
-    else if (playerSelection === aiSelection)
-    {
-        aiSelection = getAIChoice();
-    }
     }
 }
 
 function gameOn()
 {
-    for (let i = 0; i < 5; i++) 
-    {
-        let playerSelection = prompt("Player Selection: ","Rock, Paper, or Scissors");
-        //If selection is null
-        if (typeof(playerSelection === null))
+    for (let i = 0; i < 5; i++) {
+        
+        while (true) 
         {
-            console.log("1")
-            while (true)
+            //Ask for input
+            let playerSelection = prompt("Player Selection: ","Rock, Paper, or Scissors");
+            //Null input
+            if (playerSelection === null) 
             {
-                let playerSelection = prompt("Player Selection: ","Rock, Paper, or Scissors");
-                if (typeof(playerSelection === String))
+                console.log("Invalid selection please try again!");
+            }
+            //Valid Input
+            else if (playerSelection.toLowerCase() === "rock" ||
+            playerSelection.toLowerCase() === "paper" ||
+            playerSelection.toLowerCase() === "scissors")
+            {
+                playRound(playerSelection,getAIChoice());
+                console.log(`Player won ${playerCount} rounds.`)
+                console.log(`Computer won ${aiCount} rounds.`)
+                if (aiCount === 3)
                 {
-                    console.log(typeof(playerSelection))
-                    break;
+                    console.log("Computer wins the game!");
+                    return
                 }
-                else
-                {   
-                    console.log(typeof(playerSelection))
-                    console.log("Please enter your choice!")
+                else if (playerCount === 3)
+                {
+                    console.log("Player wins the game!");
+                    return
                 }
+                break;
             }
         }
-        //Valid selection
-        else if ((playerSelection.toLowerCase() === ("rock" || "paper" || "scissors"))
-                    && (typeof(playerSelection) === String))
-        {
-            console.log("valid selection")
-            break;
-        }
-        playRound(playerSelection,getAIChoice());
     }
 }
 gameOn()
